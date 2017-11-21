dc005bb6395c44f6718e8a6780119f763250a4f8
manudee <manudee@gmail.com>
2017-11-21 13:46:21 -0500

    added post route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,5 +7,11 @@ module.exports = function(app){
 	app.get("/api/friends", function(req, res) {
 		res.json(friends);
 	});
+	
+	app.post("api/friends", function(req,res){
+
+		var friendAdded = req.body;
+		res.json(friendAdded);
+	});
 }
 
