a8f17a909200612ae7effbc07a893c9d5a4556a4
Manasi <manudee@gmail.com>
2017-11-20 23:20:11 -0500

    Added get routes and html files

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+var friends = require('../data/friend.js');
+
+
+module.exports = function(app){
+
+
+	app.get("/api/friends", function(req, res) {
+		res.json(friends);
+	});
+}
+
--- server.js
+++ server.js
@@ -6,20 +6,20 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-var friends = require('./app/data/friend.js');
+// var friends = require('../data/friend.js');
 //sets up the express app to handle data parsing
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
 
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
-//displaying friends attributes
-console.log('friends ' + friends[0].name);
-console.log('friends ' + friends[0].photo);
-console.log('friends ' + friends[0].scores);
+// //displaying friends attributes
+// console.log('friends ' + friends[0].name);
+// console.log('friends ' + friends[0].photo);
+// console.log('friends ' + friends[0].scores);
 
 
 app.listen(PORT, function() {
