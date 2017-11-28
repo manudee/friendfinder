f6df1870e275cefea07c0503ddf9fdfb7a20c51b
Manasi <manudee@gmail.com>
2017-11-27 22:21:31 -0500

    Added images for friend

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,5 +1,5 @@
 var friends = [{
-  "name":"deepika",
+  "name":"Deepika Padukone",
   "photo":"https://static.toiimg.com/photo/60956683.cms",
   "scores":[
       5,
@@ -16,16 +16,66 @@ var friends = [{
 },
 
 {
-  "name":"ranbir",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Ranbir Kapoor",
+  "photo":"http://www.bollywoodlife.com/wp-content/uploads/2016/11/Ranbie-Kapoor.jpg",
   "scores":[
       2,
-      5,
+      1,
       3,
       2,
-      1,
+      5,
       4,
       3,
+      1,
+      4,
+      1
+    ]
+},
+
+{
+  "name":"Anushka Sharma",
+  "photo":"https://www.hdwallpapers.in/walls/anushka_sharma_2015-wide.jpg",
+  "scores":[
+      2,
+      3,
+      1,
+      4,
+      2,
+      4,
+      3,
+      2,
+      4,
+      1
+    ]
+},
+
+{
+  "name":"Virat Kohli",
+  "photo":"https://pbs.twimg.com/media/DITNEZTVoAAyt5v.jpg",
+  "scores":[
+      2,
+      2,
+      3,
+      3,
+      1,
+      2,
+      3,
+      1,
+      5,
+      1
+    ]
+},
+{
+  "name":"Alia Bhatt",
+  "photo":"http://www.whatsappdunia.com/uploads/images/594ce397b4bb8.jpg",
+  "scores":[
+      2,
+      1,
+      3,
+      1,
+      1,
+      4,
+      2,
       1,
       5,
       1
@@ -33,119 +83,69 @@ var friends = [{
 },
 
 {
-  "name":"anushka",
-  "photo":"https://static.toiimg.com/photo/60956683.cms",
+  "name":"Parineet Chopra",
+  "photo":"http://www.shortday.in/wp-content/uploads/2015/02/Parineeti-Chopra-sensation-Bollywood.jpg",
   "scores":[
       2,
-      4,
-      1,
-      4,
-      2,
-      4,
-      3,
-      2,
-      2,
-      1
-    ]
-},
-
-{
-  "name":"virat",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
-      2,
-      2,
-      3,
-      3,
-      1,
-      3,
-      3,
-      1,
       5,
-      1
-    ]
-},
-{
-  "name":"alia",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
+      5,
       2,
       1,
-      3,
-      1,
-      1,
       4,
-      1,
-      1,
+      4,
+      2,
       5,
       1
     ]
 },
 
 {
-  "name":"parineeti",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Rahul Bose",
+  "photo":"http://sim04.in.com/ed66dd36cb109f53775fc33515a0846e_m.jpg",
   "scores":[
       2,
       5,
-      4,
-      2,
-      1,
-      4,
-      4,
-      1,
+      3,
       5,
+      1,
+      3,
+      3,
+      1,
+      3,
       1
     ]
 },
 
 {
-  "name":"rahul",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Milind Soman",
+  "photo":"http://media2.intoday.in/indiatoday/images/stories//2015September/cover-story-2_mos_092815035749.jpg",
   "scores":[
-      2,
+      1,
       5,
       3,
       2,
       1,
       4,
-      3,
+      2,
       1,
-      5,
+      2,
       1
     ]
 },
 
 {
-  "name":"milind",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Vidya Balan",
+  "photo":"http://img01.ibnlive.in/ibnlive/uploads/2017/04/vidya-balan1.jpg",
   "scores":[
       2,
-      5,
       3,
+      2,
       2,
       1,
       4,
       3,
       1,
-      5,
-      1
-    ]
-},
-
-{
-  "name":"vidya",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-  "scores":[
       2,
-      5,
-      3,
-      2,
-      1,
-      4,
-      3,
-      1,
-      5,
       1
     ]
 },
